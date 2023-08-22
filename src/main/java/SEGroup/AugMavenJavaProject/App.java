565709d248b7d0725fb578ae35da5a1f84a9e9b2
Yogita <agyogita@gmail.com>
2023-08-22 21:15:48 +0530

    First commit to Maven Java project on Aug

---
+++ src/main/java/SEGroup/AugMavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package SEGroup.AugMavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Every one on AUg 22. see u on 23 Aug 2023" );
+    }
+}
